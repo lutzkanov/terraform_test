82c2d0e15c245784c5d8fde30c89531a5664b4bc
mlutzkan <mlutzkan@gmail.com>
2025-02-16 15:12:42 +0200

    blaaa

--- main.tf
+++ main.tf
@@ -11,21 +11,25 @@ variable "aws_secret_access_key" {
 }
 
 variable "aws_region" {
-  default = "ap-south-1"
+  description = "AWS Region"
+  type        = string
+  default     = "ap-south-1"
 }
 
 variable "bucket_name" {
-  default = "test"
+  description = "S3 Bucket Name"
+  type        = string
+  default     = "test"
 }
 
-
-
+# AWS Provider Configuration
 provider "aws" {
-  region = var.aws_region
+  region     = var.aws_region
   access_key = var.aws_access_key_id
   secret_key = var.aws_secret_access_key
 }
 
+# S3 Bucket Resource
 resource "aws_s3_bucket" "website_bucket" {
   bucket = var.bucket_name
   acl    = "public-read"
@@ -33,20 +37,27 @@ resource "aws_s3_bucket" "website_bucket" {
   website {
     index_document = "index.html"
   }
-
-  block_public_acls = false
-  ignore_public_acls = false
-  block_public_policy = false
-  ignore_changes = [bucket]
 }
 
-resource "aws_s3_bucket_object" "website_files" {
-  for_each = fileset("./quizz-app", "**/*")
-
+# S3 Bucket Public Access Block (new resource for managing public access)
+resource "aws_s3_bucket_public_access_block" "website_bucket_public_access" {
   bucket = aws_s3_bucket.website_bucket.bucket
-  key    = each.value
-  source = "./quizz-app/${each.value}"
-  acl    = "public-read"
+
+  block_public_acls   = false
+  ignore_public_acls  = false
+  block_public_policy = false
+}
+
+# S3 Bucket Object (Website Files)
+resource "aws_s3_bucket_object" "website_files" {
+  for_each = fileset("./quizz-app", "**/*")  # Loops through the files in 'quizz-app'
+
+  bucket      = aws_s3_bucket.website_bucket.bucket
+  key         = each.value
+  source      = "./quizz-app/${each.value}"
+  acl         = "public-read"
+
+  # Content-Type based on file extension
   content_type = lookup({
     html = "text/html",
     css  = "text/css",
